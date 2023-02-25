d894ae3b60eee7ee47812ce87a6637260a976cf3
Shady Ramadan <89228710+ShadyRamadan888@users.noreply.github.com>
2023-02-25 20:31:47 +0200

    Still-Login-SharedPreference

--- app/src/main/java/com/example/agriclutureassistant/ProjectData.java
+++ app/src/main/java/com/example/agriclutureassistant/ProjectData.java
@@ -3,4 +3,8 @@ package com.example.agriclutureassistant;
 public class ProjectData {
     public final static String BaseUrlTemper="https://api.weatherapi.com";
     public final static String api_key="f40578275d2b4e0db0b190851231502";
+    //SharedPreference Data
+    public final static String filename="login2";
+    public final static String email_="email";
+    public final static String password ="password";
 }
--- app/src/main/java/com/example/agriclutureassistant/ui/fragments/HomePage.java
+++ app/src/main/java/com/example/agriclutureassistant/ui/fragments/HomePage.java
@@ -2,13 +2,20 @@ package com.example.agriclutureassistant.ui.fragments;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 
+import com.example.agriclutureassistant.ProjectData;
 import com.example.agriclutureassistant.R;
+import com.example.agriclutureassistant.ui.MainActivity;
 
 public class HomePage extends AppCompatActivity {
+
+    SharedPreferences sharedPreferences;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +36,12 @@ public class HomePage extends AppCompatActivity {
         });
 
 
+
+        sharedPreferences = getSharedPreferences(ProjectData.filename, Context.MODE_PRIVATE);
+        if (sharedPreferences.contains(ProjectData.email_)){
+
+            startActivity(new Intent(HomePage.this, MainActivity.class));
+        }
     }
 
 }
