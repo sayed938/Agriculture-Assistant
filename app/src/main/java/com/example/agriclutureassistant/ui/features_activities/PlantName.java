7afaab00c569152bedd8180e5f5785ff190f90d7
sayed_samir <sayed.samir6060@gmail.com>
2023-05-05 14:24:35 +0300

    sayed-test2

--- app/src/main/java/com/example/agriclutureassistant/ui/features_activities/PlantName.java
+++ app/src/main/java/com/example/agriclutureassistant/ui/features_activities/PlantName.java
@@ -1,16 +1,43 @@
 package com.example.agriclutureassistant.ui.features_activities;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.example.agriclutureassistant.R;
 
 public class PlantName extends AppCompatActivity {
-
+ImageView bt_camera;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plant_name);
+        bt_camera=findViewById(R.id.bt_cameraPlantName);
+        bt_camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent,100);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(RESULT_OK==100&&resultCode==RESULT_OK) {
+            Toast.makeText(this, "Success image ", Toast.LENGTH_SHORT).show();
+        }
     }
 }
